14057d902318cae348e437be2649d0722600f6e8
Simone Busoli <simone.busoli@gmail.com>
2018-09-01 01:12:44 +0200

    Add scripts for publishing

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import createAction from './createAction'
+import createMutation from './createMutation'
+import mapActions from './mapActions'
+import mapMutations from './mapMutations'
+
+export {createAction, createMutation, mapActions, mapMutations}
